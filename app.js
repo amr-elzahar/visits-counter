65827ea3897837dec54a9531962560c287ce62fa
Amr Elzahar <amr92621@gmail.com>
2023-03-30 22:31:55 +0200

    root commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+
+const redisClient = redis.createClient({
+  url: "redis://redis:6379",
+});
+
+redisClient.on("error", () => {
+  console.log("An error occured");
+});
+
+redisClient.on("connect", () => {
+  console.log("SUCCESSFULLY CONNECTED");
+});
+
+redisClient.connect();
+
+app.get("/", async (req, res) => {
+  try {
+    const keyExists = await redisClient.exists("visits");
+
+    if (keyExists) {
+      const value = await redisClient.get("visits");
+      res.send(`Number of visits: ${value}`);
+      await redisClient.set("visits", parseInt(value) + 1);
+    } else {
+      await redisClient.set("visits", 0);
+      const value = await redisClient.get("visits");
+      res.send(`Number of visits: ${value}`);
+      await redisClient.set("visits", parseInt(value) + 1);
+    }
+  } catch (error) {
+    console.log(error.message);
+    return res.send("Something went wrong!");
+  }
+});
+
+app.listen(3000, () => console.log("App listenning on port 3000"));
---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+if [ ! -d "${PWD}/redis-data" ]; then mkdir -p "${PWD}/redis-data"; fi
